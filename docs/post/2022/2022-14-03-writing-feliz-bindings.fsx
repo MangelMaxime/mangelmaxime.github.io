73e802fb43da4b11f2741b00d6ae37a039a74572
Maxime Mangel <mangel.maxime@protonmail.com>
2022-03-20 14:43:41 +0100

    Save WIP

---
+++ docs/post/2022/2022-14-03-writing-feliz-bindings.fsx
@@ -0,0 +1,38 @@
+(**
+---
+layout: standard
+---
+*)
+
+(*** hide ***)
+
+#r "nuget: Fable.Core, 3.6.2"
+#r "nuget: Feliz, 1.61.0"
+
+(**
+:::info
+This article has been written using Fable.Core 3.6.2 and Feliz 1.61.0.
+:::
+*)
+
+(*** hide ***)
+
+open Feliz
+open Fable.Core
+open Fable.Core.JsInterop
+
+(**
+
+In this article, we are going to learn how to write an F# binding for a React library.
+
+To do so, we are going to use [react-resize-detector](https://www.npmjs.com/package/react-resize-detector/v/7.0.0) as the library to be bound.
+
+## Feliz API
+
+## Hooks
+
+## High Order Components
+
+## ReactComponent API
+
+**)
