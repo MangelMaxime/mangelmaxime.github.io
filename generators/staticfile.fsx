c21ea8098c5f221c8242f430ebf82b1f7d9aa3f6
Maxime Mangel <mangel.maxime@protonmail.com>
2022-09-30 22:48:45 +0200

    Init hugo

--- generators/staticfile.fsx
+++
@@ -1,7 +0,0 @@
-#r "./src/FSharp.Static/bin/Debug/net6.0/FSharp.Static.Core.dll"
-
-open System.IO
-
-let generate (ctx: SiteContents) (projectRoot: ProjectRoot.T) (page: string) =
-    let inputPath = Path.Combine(projectRoot, page)
-    File.ReadAllBytes inputPath
